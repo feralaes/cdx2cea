22850e77f6adf44208d016f9e95ad68b289014dc
Fernando Alarid-Escudero <feralaes@users.noreply.github.com>
2021-05-11 10:59:56 -0500

    Update _master.R
    Add code to instaLL iMIS from CRAN's archive

--- analysis/_master.R
+++ analysis/_master.R
@@ -19,6 +19,19 @@
 #### 00 Install and load packages ####
 ### Uncomment if you don't have all required packages installed
 # source("analysis/app0_package_setup.R", echo = TRUE) 
+## Install IMIS
+url <- "https://cran.r-project.org/src/contrib/Archive/IMIS/IMIS_0.1.tar.gz"
+pkgFile <- "IMIS_0.1.tar.gz"
+download.file(url = url, destfile = pkgFile)
+
+# Install dependencies
+install.packages(c("ada", "ipred", "evd"))
+
+# Install package
+install.packages(pkgs=pkgFile, type="source", repos=NULL)
+
+# Delete package tarball
+unlink(pkgFile)
 
 #### 01 Load inputs ####
 source("analysis/01_model_inputs.R", echo = TRUE)
